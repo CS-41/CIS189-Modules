1c0e1f3101081c5588e443ee03165a1c177f5f4c
CS-41 <66191434+CS-41@users.noreply.github.com>
2020-06-03 23:35:34 -0500

    Add files via upload

--- test_functions.py
+++ test_functions.py
@@ -1,9 +1,21 @@
+"""
+Program: test_functions.py
+Author: Elizabeth Allen
+Last date modified: 06/03/2020
+
+The purpose of this program is to show the preliminary test file.
+The input is 3.
+The output will be an error, per the instructions.
+"""
+
+
 import unittest
-from main import camper_age_input
+from main.camper_age_input_prelim import convert_to_months
 
 class FunctionTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, True)
+    def test_months(self):
+         months = convert_to_months(3)
+         self.assertEqual(3)
 
 
 if __name__ == '__main__':
